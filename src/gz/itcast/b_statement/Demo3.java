4466dc1cdf8a556702cf4f1a67f619f70f3e7416
ZP <634001184@qq.com>
2019-07-17 12:15:24 +0800

    update

--- src/gz/itcast/b_statement/Demo3.java
+++ src/gz/itcast/b_statement/Demo3.java
@@ -1,4 +1,65 @@
 package gz.itcast.b_statement;
 
+import gz.itcast.util.JdbcUtil;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * 使用Statement执行DQL语句（查询操作）
+ * @author zp
+ *
+ */
 public class Demo3 {
+
+    @Test
+    public void test1(){
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            //获取连接
+            conn = JdbcUtil.getConnection();
+            //创建Statement
+            stmt = conn.createStatement();
+            //准备sql
+            String sql = "SELECT * FROM student";
+            //执行sql
+            ResultSet rs = stmt.executeQuery(sql);
+           //移动光标
+			/*boolean flag = rs.next();
+
+			flag = rs.next();
+			flag = rs.next();
+			if(flag){
+				//取出列值
+				//索引
+				int id = rs.getInt(1);
+				String name = rs.getString(2);
+				String gender = rs.getString(3);
+				System.out.println(id+","+name+","+gender);
+
+				//列名称
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				String gender = rs.getString("gender");
+				System.out.println(id+","+name+","+gender);
+			}*/
+
+			//遍历结果
+            while (rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String gender = rs.getString("gender");
+                System.out.println(id+","+name+","+gender);
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }finally {
+            JdbcUtil.close(conn,stmt);
+        }
+    }
 }
